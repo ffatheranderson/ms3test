f01b9396cf4bfb4e8ab1b13bfdf575262696f898
PavelSapronov <ffatheranderson@gmail.com>
2019-01-02 17:37:51 -0600

    Initial commit.

---
+++ management-rest-api/src/main/java/sapronov/pavel/managementrestapi/ManagementRestApiApplication.java
@@ -0,0 +1,14 @@
+package sapronov.pavel.managementrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementRestApiApplication.class, args);
+	}
+
+}
+
